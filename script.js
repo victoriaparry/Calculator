a6e1e147a3664bbf9bee226afad65f68211295be
Victoria <vicytoriaparry@gmail.com>
2023-09-23 16:39:40 -0700

    update

--- script.js
+++ script.js
@@ -3,18 +3,22 @@ let op = document.querySelectorAll(".operator");
 let clear = document.querySelector("#clear");
 let equals = document.querySelector("#equals");
 let userInput = document.querySelector("#user-input");
+let first = 0;
+let last = 0;
+let displayVal = 0;
+
 
 let inputList = [];
 
 
-clear.addEventListener("click", function clear() {
-    inputList = [];
-    calcDisplay();
+clear.addEventListener("click", function (){
+    clearDisplay();
 });
 
-equals.addEventListener("click", function alertme() {
-    alert("equals");
-});
+function clearDisplay() {
+    inputList = [];
+    calcDisplay();
+}
 
 
 for (let i = 0; i < number.length; i++) {
@@ -32,7 +36,41 @@ for (let i = 0; i < op.length; i++) {
 }
 
 console.log(Array.isArray(inputList))
+console.log(inputList)
 
 function calcDisplay(){
-    userInput.innerText = inputList.join('');
+    userInput.textContent = inputList.join('');
+}
+
+equals.addEventListener("click", function(){calculate(inputList)});
+
+function calculate(list){
+    for(let i = 0; i < list.length; i++){
+        if (list[i] == "+"){
+            first = parseInt(list.slice(0,i).join(''));
+            last = parseInt(list.slice(i+1, (list.length)).join(''));
+            console.log(first+last);
+            displayVal = first + last;
+        }
+        if (list[i] == "-"){
+            first = parseInt(list.slice(0,i).join(''));
+            last = parseInt(list.slice(i+1, (list.length)).join(''));
+            console.log(first-last);
+            displayVal = first - last;
+        }
+        if (list[i] == "x"){
+            first = parseInt(list.slice(0,i).join(''));
+            last = parseInt(list.slice(i+1, (list.length)).join(''));
+            console.log(first*last);
+            displayVal = first * last;
+        }
+        if (list[i] == "/"){
+            first = parseInt(list.slice(0,i).join(''));
+            last = parseInt(list.slice(i+1, (list.length)).join(''));
+            console.log(first/last);
+            displayVal = first/last;
+        }
+        clearDisplay()
+        userInput.textContent = displayVal;
+    }
 }
